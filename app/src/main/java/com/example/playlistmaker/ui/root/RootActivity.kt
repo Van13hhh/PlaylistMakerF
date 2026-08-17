7143db60a6df6ef2d4ed608b927b6da93b76bb13
Van13hhh <novikovivan06@gmail.com>
2026-08-17 22:19:12 +0300

    Adding a feature to show playlist, delete, share, edit and update app launcher

--- app/src/main/java/com/example/playlistmaker/ui/root/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/root/RootActivity.kt
@@ -57,6 +57,11 @@ class RootActivity : AppCompatActivity() {
                     binding.divider.isVisible = false
                 }
 
+                R.id.playlistEditFragment -> {
+                    bottomNavigationView.isVisible = false
+                    binding.divider.isVisible = false
+                }
+
                 else -> {
                     bottomNavigationView.isVisible = true
                     binding.divider.isVisible = true
